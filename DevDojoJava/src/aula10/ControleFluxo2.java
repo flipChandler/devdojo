78bdaa89f326683f8d5744bfe6d0762a2f637273
Felipe Santos <felipe.santos11@hotmail.com>
2020-05-15 02:40:55 -0300

    ternario e exercicio de if/else

---
+++ DevDojoJava/src/aula10/ControleFluxo2.java
@@ -0,0 +1,19 @@
+package aula10;
+
+import java.util.Scanner;
+
+public class ControleFluxo2 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int idade = 0;
+		String status;
+
+		System.out.println("Digite a sua idade: ");
+		idade = scanner.nextInt();
+
+		status = idade >= 18 ? "Já é adulto!" : "Ainda está nas fraldas!";		
+		
+		System.out.println(status);
+	}
+}
---
+++ DevDojoJava/src/aula10/ExercicioFluxo.java
@@ -0,0 +1,37 @@
+package aula10;
+
+import java.util.Scanner;
+
+public class ExercicioFluxo {
+
+	public static void main(String[] args) {
+		/*
+		 * SALARIO < 1000 -> IMPOSTO DE 5%
+		 * SALARIO >= 1000 && SALARIO < 2000 -> IMPOSTO DE 10%
+		 * SALARIO >= 2000 && SALARIO < 4000 -> IMPOSTO DE 15%
+		 * SALARIO >= 4000 -> IMPOSTO DE 20%
+		 */
+		
+		Scanner scanner = new Scanner(System.in);
+		double salario, imposto = 0d;
+		
+		System.out.println("Informe o seu salário: ");
+		salario = scanner.nextDouble();
+		
+		if (salario < 1000) {
+			imposto = 5/100d;
+		}else if (salario >= 1000 && salario < 2000) {
+			imposto = 10/100d;
+		}else if ( salario >= 2000 && salario < 4000) {
+			imposto = 15/100d;
+		}else {
+			imposto = 20/100d;
+		}
+		
+		imposto *= salario;
+		
+		System.out.printf("O seu salário é %.2f e o total de impostos a pagar é %.2f", salario, imposto);
+		
+	}
+
+}

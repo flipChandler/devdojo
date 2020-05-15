215053c61d6da2bf983039a60688cc421b375fb4
Felipe Santos <felipe.santos11@hotmail.com>
2020-05-15 00:48:51 -0300

    exemplo de javadoc

---
+++ DevDojoJava/src/aula05/ImprimindoVariaveis.java
@@ -0,0 +1,14 @@
+package aula05;
+
+public class ImprimindoVariaveis {
+	
+	/***	
+	 * esse é o javadocs
+	 * @param args parametro de entrada do metodo main
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

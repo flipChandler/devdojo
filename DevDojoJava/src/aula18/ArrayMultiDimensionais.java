e6ab08b48d1d8cf5b7aae55bc9d094ca8ce684f9
Felipe Santos <felipe.santos11@hotmail.com>
2020-05-17 02:01:13 -0300

    array bi-dimensional com forEach

---
+++ DevDojoJava/src/aula18/ArrayMultiDimensionais.java
@@ -0,0 +1,31 @@
+package aula18;
+
+public class ArrayMultiDimensionais {
+
+	public static void main(String[] args) {
+		int[][] dias = new int[2][2];
+		
+		dias[0][0] = 30;
+		dias[0][1] = 31;
+		dias[1][0] = 29;
+		dias[1][1] = 28;
+		
+		//SE TEMOS UM ARRAY BI-DIMENSIONAL, PRECISAMOS DE 2 FOR'S
+		
+		for (int i = 0; i < dias.length;i++) {
+			for (int j = i; j < dias[i].length;j++) {
+				System.out.println(dias[i][j]);
+			}
+		}
+		
+		System.out.println("PULA LINHA");
+		
+		for (int[] ref : dias) {// REFERENCIA DA MEMORIA DO ARRAY
+			for ( int dia : ref) { // PERCORRE A CADA REFERENCIA
+				System.out.println(dia);
+			}
+		}
+
+	}
+
+}

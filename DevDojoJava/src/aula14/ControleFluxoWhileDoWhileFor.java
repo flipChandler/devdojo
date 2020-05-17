be9e1d2f8617ea71e2c291af6b91aae26c60bcc8
Felipe Santos <felipe.santos11@hotmail.com>
2020-05-16 23:36:07 -0300

    loop while, do/while e for

---
+++ DevDojoJava/src/aula14/ControleFluxoWhileDoWhileFor.java
@@ -0,0 +1,26 @@
+package aula14;
+
+public class ControleFluxoWhileDoWhileFor {
+
+	public static void main(String[] args) {
+		int contador = 0;
+		
+		while(contador < 10) {
+			System.out.println(contador++);			
+		}
+		
+		System.out.println("PULA LINHA");
+		
+		contador = 0;
+		do {
+			System.out.println(contador++);
+		}while(contador < 10);
+		
+		System.out.println("PULA LINHA");		
+		
+		for (contador = 0; contador < 10; contador++) {
+			System.out.println(contador);
+		}
+	}
+
+}

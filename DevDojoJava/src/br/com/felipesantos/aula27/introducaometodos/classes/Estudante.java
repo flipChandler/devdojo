2cc019916bb76b2abe3d3783e7fb40f52df85073
Felipe Santos <felipe.santos11@hotmail.com>
2020-05-19 01:37:21 -0300

    getter, setter e tratamento de erros

--- DevDojoJava/src/br/com/felipesantos/aula27/introducaometodos/classes/Estudante.java
+++ DevDojoJava/src/br/com/felipesantos/aula27/introducaometodos/classes/Estudante.java
@@ -1,17 +1,47 @@
 package br.com.felipesantos.aula27.introducaometodos.classes;
 
 public class Estudante {
-	public String nome;
-	public int idade;
-	public double[] notas;
+	private String nome;
+	private int idade;
+	private double[] notas;
 	
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		if (idade < 0) {
+			System.out.println("Você não pode fazer isso!");
+			return;
+		}
+		
+		this.idade = idade;
+	}
+
+	public double[] getNotas() {
+		return notas;
+	}
+
+	public void setNotas(double[] notas) {
+		this.notas = notas;
+	}
+
 	public void imprimeDados() {
 		System.out.println("Nome: " + this.nome);
 		System.out.println("Idade: " + this.idade);
 		System.out.print("Notas: ");
 		
-		if(notas != null) {
-			for (double nota : notas) {			
+		if(this.notas != null) {
+			for (double nota : this.notas) {			
 				System.out.print(nota + " ");
 			}
 		}
@@ -20,16 +50,16 @@ public class Estudante {
 	}
 	
 	public void tirarMedia() {
-		if (notas == null) {
+		if (this.notas == null) {
 			System.out.println("Esse aluno não possui notas!");
 			return; // break em if/else statements
 		}		
 		double media = 0;
 		
-		for (double nota : notas) {
+		for (double nota : this.notas) {
 			media += nota;
 		}		
-		media /= notas.length;
+		media /= this.notas.length;
 		
 		if (media > 6) {
 			System.out.println("\nA média é " + media + " situação: Aprovado");
--- DevDojoJava/src/br/com/felipesantos/aula27/introducaometodos/test/EstudanteTest.java
+++ DevDojoJava/src/br/com/felipesantos/aula27/introducaometodos/test/EstudanteTest.java
@@ -1,5 +1,6 @@
 package br.com.felipesantos.aula27.introducaometodos.test;
 
+import java.util.Arrays;
 import java.util.Random;
 
 import br.com.felipesantos.aula27.introducaometodos.classes.Estudante;
@@ -8,14 +9,16 @@ public class EstudanteTest {
 
 	public static void main(String[] args) {
 		Estudante estudante = new Estudante();
+		double[] notas = new double[3];
 		
-		estudante.nome = "João das Couves";
-		estudante.idade = 33;	
-		estudante.notas = new double[3];
+		estudante.setNome("João das Couves");
+		estudante.setIdade(33);	
+		estudante.setNotas(notas);
 		
 		for (int i = 0; i < 3;i++) {
 			Random random = new Random();
-			estudante.notas[i] = 10 * random.nextDouble();
+			notas[i] = 10 * random.nextDouble();
+			estudante.setNotas(notas);
 		}
 		
 		estudante.imprimeDados();

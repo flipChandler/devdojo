74c0d18a7b588df2d3c632f3dd7f0584be73f532
Felipe Santos <felipe.santos11@hotmail.com>
2020-05-19 23:23:29 -0300

    bloco de inicializacao | array inicializa com 100

--- DevDojoJava/src/br/com/felipesantos/blocosdeinicializacao/Cliente.java
+++ DevDojoJava/src/br/com/felipesantos/blocosdeinicializacao/Cliente.java
@@ -1,14 +1,25 @@
 package br.com.felipesantos.blocosdeinicializacao;
 
 public class Cliente {
-	private int[] parcelas = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
-	
-	public Cliente() {
-		System.out.println("Dentro do construtor");
-		for (int parcela : this.parcelas) {
-			System.out.print(parcela + " ");
+	/*
+	 * 1 - Alocado espaço na memoria para o objeto que será criado 2 - Cada atributo
+	 * de classe é criado e inicializado com seus valores default ou valores
+	 * explicitos 3 - Bloco de inicialização é executado 4 - O construtor é
+	 * executado
+	 */
+	private int[] parcelas;
+
+	{
+		System.out.println("Dentro do bloco de inicialização");
+		parcelas = new int[100];
+		for (int i = 1; i <= 100; i++) {
+			parcelas[i - 1] = i;
 		}
 		System.out.println();
+	}
+
+	public Cliente() {
+
 	}
 
 	public int[] getParcelas() {
@@ -18,7 +29,5 @@ public class Cliente {
 	public void setParcelas(int[] parcelas) {
 		this.parcelas = parcelas;
 	}
-	
-	
-	
+
 }

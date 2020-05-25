3d1be2b97a2d576b5994e9f977958de25a5d2f9d
Felipe Santos <felipe.santos11@hotmail.com>
2020-05-25 01:28:18 -0300

    heranca com atributo protected

--- DevDojoJava/src/br/com/felipesantos/javacore/heranca/classes/Funcionario.java
+++ DevDojoJava/src/br/com/felipesantos/javacore/heranca/classes/Funcionario.java
@@ -6,6 +6,11 @@ public class Funcionario extends Pessoa{ // FUNCIONARIO É-UMA PESSOA
 	public void print() {
 		super.print();// método da superclasse Pessoa
 		System.out.println("Salario: " + this.salario);
+		imprimeReciboPagamento();
+	}
+	
+	public void imprimeReciboPagamento() {
+		System.out.println("Eu, " + super.nome + " recebi o pagamento de " + this.salario);
 	}
 	
 	public double getSalario() {
--- DevDojoJava/src/br/com/felipesantos/javacore/heranca/classes/Pessoa.java
+++ DevDojoJava/src/br/com/felipesantos/javacore/heranca/classes/Pessoa.java
@@ -1,9 +1,9 @@
 package br.com.felipesantos.javacore.heranca.classes;
 
 public class Pessoa {
-	private String nome;
-	private String cpf;
-	private Endereco endereco;
+	protected String nome;
+	protected String cpf;
+	protected Endereco endereco;
 	
 	public void print() {
 		System.out.println("Nome: " + this.nome);

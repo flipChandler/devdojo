9e991dc0fe23b7500550dbb9ff4580e1c5fe6c14
Felipe Santos <felipe.santos11@hotmail.com>
2020-05-27 14:02:30 -0300

    generalizando metodo na classe abstrata Funcionario

--- DevDojoJava/src/br/com/felipesantos/javacore/polimorfismo/classes/RelatorioPagamento.java
+++ DevDojoJava/src/br/com/felipesantos/javacore/polimorfismo/classes/RelatorioPagamento.java
@@ -2,19 +2,29 @@ package br.com.felipesantos.javacore.polimorfismo.classes;
 
 public class RelatorioPagamento {
 	
-	public void pagamentoGerente(Gerente gerente) {
-		System.out.println("Gerando relatorio de pagamento para gerência");
-		gerente.calcularPagamento();
-		System.out.println("Nome: " + gerente.getNome());
-		System.out.println("Salário desse mês: " + gerente.getSalario());
-		
-	}
+//	public void pagamentoGerente(Gerente gerente) {
+//		System.out.println("Gerando relatorio de pagamento para gerência");
+//		gerente.calcularPagamento();
+//		System.out.println("Nome: " + gerente.getNome());
+//		System.out.println("Salário desse mês: " + gerente.getSalario());
+//		
+//	}
+//	
+//	public void pagamentoVendedor(Vendedor vendedor) {
+//		System.out.println("Gerando relatorio de pagamento para vendedor");
+//		vendedor.calcularPagamento();
+//		System.out.println("Nome: " + vendedor.getNome());
+//		System.out.println("Salário desse mês: " + vendedor.getSalario());
+//		
+//	}
 	
-	public void pagamentoVendedor(Vendedor vendedor) {
-		System.out.println("Gerando relatorio de pagamento para vendedor");
-		vendedor.calcularPagamento();
-		System.out.println("Nome: " + vendedor.getNome());
-		System.out.println("Salário desse mês: " + vendedor.getSalario());
-		
+	// ESSE MÉTODO FUNCIONA PARA GERENTE E VENDEDOR | 
+	// QNDO PASSAR O GERENTE, calcularPagamento vai ser chamado de Gerente
+	//QNDO PASSAR O VENDEDOR, calcularPagamento vai ser chamado de Vendedor
+	public void pagamentoGenerico(Funcionario funcionario) {
+		System.out.println("-----Gerando relatorio de pagamento-----");
+		funcionario.calcularPagamento();
+		System.out.println("Nome: " + funcionario.getNome());
+		System.out.println("Salário desse mês: " + funcionario.getSalario());
 	}
 }
--- DevDojoJava/src/br/com/felipesantos/javacore/polimorfismo/test/PolimorfismoTest.java
+++ DevDojoJava/src/br/com/felipesantos/javacore/polimorfismo/test/PolimorfismoTest.java
@@ -1,5 +1,6 @@
 package br.com.felipesantos.javacore.polimorfismo.test;
 
+import br.com.felipesantos.javacore.polimorfismo.classes.Funcionario;
 import br.com.felipesantos.javacore.polimorfismo.classes.Gerente;
 import br.com.felipesantos.javacore.polimorfismo.classes.RelatorioPagamento;
 import br.com.felipesantos.javacore.polimorfismo.classes.Vendedor;
@@ -10,10 +11,18 @@ public class PolimorfismoTest {
 		Gerente gerente = new Gerente("Milton", 5000, 2000);
 		Vendedor vendedor = new Vendedor("Yuri", 2000, 20000);
 		RelatorioPagamento relatorio = new RelatorioPagamento();
-		relatorio.pagamentoGerente(gerente);
-		System.out.println("---------------------------------------");
-		relatorio.pagamentoVendedor(vendedor);
+//		relatorio.pagamentoGerente(gerente);
+//		System.out.println("---------------------------------------");
+//		relatorio.pagamentoVendedor(vendedor);
 		
+		relatorio.pagamentoGenerico(gerente);
+		System.out.println("-----------------");
+		relatorio.pagamentoGenerico(vendedor);
+		
+		
+		Funcionario funcionario = gerente;
+		System.out.println("------------------");
+		System.out.println(funcionario.getSalario());
 	}
 
 }

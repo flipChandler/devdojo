49c3a0c3a0cb8cc749d88fa8544fe8477c31e02a
Felipe Santos <felipe.santos11@hotmail.com>
2020-05-28 01:32:31 -0300

    throws IOException

--- DevDojoJava/src/br/com/felipesantos/javacore/exception/checkedexception/test/CheckedExceptionTest.java
+++ DevDojoJava/src/br/com/felipesantos/javacore/exception/checkedexception/test/CheckedExceptionTest.java
@@ -6,16 +6,22 @@ import java.io.IOException;
 public class CheckedExceptionTest {
 
 	public static void main(String[] args) {
-		criarArquivo();
+		try {
+			criarArquivo();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
-	public static void criarArquivo() {
+	public static void criarArquivo() throws IOException {
 		File file = new File("Teste.txt");
 		try {
 			file.createNewFile();
 			System.out.println("Arquivo criado!");
 		} catch (IOException e) {// exceção do tipo checked
 			e.printStackTrace();// apresenta a trilha da stack que deu problema
+			throw e;// relançou a IOException para quem está chamando o método
 		}
 	}
 

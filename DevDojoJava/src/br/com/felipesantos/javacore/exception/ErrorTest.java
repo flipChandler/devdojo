630c3119eaca8c349e793cc80c679b954ee9e998
Felipe Santos <felipe.santos11@hotmail.com>
2020-05-27 23:09:47 -0300

    exceptions | stackoverflow | recursividade

---
+++ DevDojoJava/src/br/com/felipesantos/javacore/exception/ErrorTest.java
@@ -0,0 +1,15 @@
+package br.com.felipesantos.javacore.exception;
+
+public class ErrorTest {
+
+	public static void main(String[] args) {
+		stackOverflowError();
+		
+	}
+	
+	public static void stackOverflowError() {
+		stackOverflowError(); // um erro de recursividade | empilhamento metodo chama ele mesmo que chama ele mesmo
+		// Exception in thread "main" java.lang.StackOverflowError 
+	}
+
+}

a487a3c549e889e88a805f00a6d8bd64e132687c
Felipe Santos <felipe.santos11@hotmail.com>
2020-05-29 00:20:09 -0300

    complemento de try com recursos | com classes implementando AutoCloseable - Java 7

--- DevDojoJava/src/br/com/felipesantos/javacore/exception/checkedexception/test/TryWithResourcesTest.java
+++ DevDojoJava/src/br/com/felipesantos/javacore/exception/checkedexception/test/TryWithResourcesTest.java
@@ -6,19 +6,22 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
 
+import br.com.felipesantos.javacore.exception.checkedexception.classes.Leitor1;
+import br.com.felipesantos.javacore.exception.checkedexception.classes.Leitor2;
+
 public class TryWithResourcesTest {
 
 	public static void main(String[] args) {
-		
+		outroLerArquivoJava7();
 	}
-	
+
 	public static void lerArquivoJava6() {
 		Reader reader = null;
 		try {
 			reader = new BufferedReader(new FileReader("text.txt"));
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
-		}finally{
+		} finally {
 			try {
 				if (reader != null) {
 					reader.close();
@@ -27,19 +30,20 @@ public class TryWithResourcesTest {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
+
 		}
 	}
-	
+
 	public static void lerArquivoJava7() throws IOException { // NÃO PRECISA DO CATCH
-		// classes instaciadas somente as que tem as interfaces closeable e autocloseable
-		try ( Reader reader = new BufferedReader(new FileReader("text.txt")) ){ 	
+		// classes instaciadas somente as que tem as interfaces closeable e
+		// autocloseable
+		try (Reader reader = new BufferedReader(new FileReader("text.txt"))) {
 		}
 	}
-	
-	public static void outroLerArquivoJava7() { 
-		try ( Reader reader = new BufferedReader(new FileReader("text.txt")) ){ 	
-		}catch(IOException e) {
+
+	public static void outroLerArquivoJava7() {
+		try (Leitor1 leitor1 = new Leitor1(); Leitor2 leitor2 = new Leitor2()) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}

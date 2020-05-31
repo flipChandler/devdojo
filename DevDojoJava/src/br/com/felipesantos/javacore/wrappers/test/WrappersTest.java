77824b4ff89d8395b052fc6f302a8df53623cd1d
Felipe Santos <felipe.santos11@hotmail.com>
2020-05-31 01:28:32 -0300

    introducao as classes wrappers

---
+++ DevDojoJava/src/br/com/felipesantos/javacore/wrappers/test/WrappersTest.java
@@ -0,0 +1,21 @@
+package br.com.felipesantos.javacore.wrappers.test;
+
+public class WrappersTest {
+
+	public static void main(String[] args) {
+		// 8 tipos primitivos do Java
+		//byte, short, int, long, float, double, char, boolean 
+		byte bytePrimitivo = 1;
+		short shortPrimitivo = 1;
+		int intPrimitivo = 10;
+		long longPrimitivo = 10L;
+		float floatPrimitivo = 10F;
+		double doublePrimitivo = 10D;
+		char charPrimitivo = 'A';
+		boolean booleanPrimitivo = true;
+		
+		
+
+	}
+
+}

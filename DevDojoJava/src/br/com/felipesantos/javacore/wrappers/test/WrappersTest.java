dbc5c8d02a1a12660c7a1d9d8c26af51ca16e9e6
Felipe Santos <felipe.santos11@hotmail.com>
2020-05-31 23:14:36 -0300

    exemplos de metodos das classes wrapper

--- DevDojoJava/src/br/com/felipesantos/javacore/wrappers/test/WrappersTest.java
+++ DevDojoJava/src/br/com/felipesantos/javacore/wrappers/test/WrappersTest.java
@@ -14,15 +14,29 @@ public class WrappersTest {
 		char charPrimitivo = 'A';
 		boolean booleanPrimitivo = true;
 		
-		Byte byteWrapper = 127;
+		Byte byteWrapper = 127;// AUTOBOXING
 		Short shortWrapper = 1;
-		Integer integerWrapper = 1;
-		Long longWrapper = 10L;
-		Float floatWrapper = 10F;
+		Integer integerWrapper = new Integer("10"); // DEPRECIADO DESDE JAVA 9
+		Long longWrapper = new Long("15"); // DEPRECIADO DESDE JAVA 9
+		Float floatWrapper = new Float("456"); // DEPRECIADO DESDE JAVA 9
 		Double doubleWrapper = 10D;
 		Character characterWrapper = 'A';
 		Boolean booleanWrapper = true;
-
+		
+		int i = integerWrapper; //UNBOXING
+		
+		String valor = "10";
+		Float valorFloat = Float.parseFloat(valor);
+		System.out.println(valorFloat);// 10
+		
+		longWrapper = Long.valueOf("100");
+		System.out.println(longWrapper); // 100
+		
+		System.out.println(Character.isDigit('5')); // true
+		System.out.println(Character.isLetter('5')); // false
+		System.out.println(Character.isLetterOrDigit('#')); // false
+		System.out.println(Character.isUpperCase('a')); // false
+		System.out.println(Character.isLowerCase('a')); // true
 	}
 
 }

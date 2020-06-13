9fc0124590806982fc1648c771065d08223c6342
Felipe Santos <felipe.santos11@hotmail.com>
2020-06-13 12:10:12 -0300

    Classe IO - codigo refatorado BufferedWriter e BufferedReader

--- DevDojoJava/src/br/com/felipesantos/javacore/io/test/BufferedTest.java
+++ DevDojoJava/src/br/com/felipesantos/javacore/io/test/BufferedTest.java
@@ -10,30 +10,52 @@ import java.io.IOException;
 public class BufferedTest {
 
 	public static void main(String[] args) {
-		File file = new File("textoBuffered.txt");
-		try {
-			FileWriter fileWriter = new FileWriter(file);
-			BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+		File file = new File("textoBuffered.txt");		
+		
+		// REFATORADO COM TRY-RESOURCES
+		try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter (file));
+			BufferedReader bufferedReader = new BufferedReader(new FileReader(file))){
 			
 			bufferedWriter.write("Escrevendo uma mensagem no arquivo");
 			bufferedWriter.newLine();
 			bufferedWriter.write("E pulando uma linha");
-			bufferedWriter.flush(); // usado somente para a escrita
-			bufferedWriter.close();
+			bufferedWriter.flush();
 			
-			FileReader fileReader = new FileReader(file);
-			BufferedReader bufferedReader = new BufferedReader(fileReader);
-			
-			String texto = null;
-			
+			String texto;
 			while((texto = bufferedReader.readLine()) != null) {
 				System.out.println(texto);
 			}
 			
-			bufferedReader.close();
 		}catch(IOException e) {
 			e.printStackTrace();
-		}
+		}		
+		
+		
+		
+		
+//		try {
+//			FileWriter fileWriter = new FileWriter(file);
+//			BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+//			
+//			bufferedWriter.write("Escrevendo uma mensagem no arquivo");
+//			bufferedWriter.newLine();
+//			bufferedWriter.write("E pulando uma linha");
+//			bufferedWriter.flush(); // usado somente para a escrita
+//			bufferedWriter.close();
+//			
+//			FileReader fileReader = new FileReader(file);
+//			BufferedReader bufferedReader = new BufferedReader(fileReader);
+//			
+//			String texto = null;
+//			
+//			while((texto = bufferedReader.readLine()) != null) {
+//				System.out.println(texto);
+//			}
+//			
+//			bufferedReader.close();
+//		}catch(IOException e) {
+//			e.printStackTrace();
+//		}
 		
 		
 

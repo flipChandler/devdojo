a2f4e22b38e1b545b379f506eb857d66095bcd20
Felipe Santos <felipe.santos11@hotmail.com>
2020-07-11 01:16:05 -0300

    Map<Consumidor, List<Produto>> map = new HashMap<>();

--- DevDojoJava/src/br/com/felipesantos/javacore/colecoes/test/ConsumidorMapTest.java
+++ DevDojoJava/src/br/com/felipesantos/javacore/colecoes/test/ConsumidorMapTest.java
@@ -1,6 +1,8 @@
 package br.com.felipesantos.javacore.colecoes.test;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import br.com.felipesantos.javacore.colecoes.classes.Consumidor;
@@ -18,14 +20,26 @@ public class ConsumidorMapTest {
 		Produto produto4 = new Produto("012", "Samsung Galaxy S7 64gb", 3250.5);
 		
 		// ASSOCIAR UM CONSUMIDOR A UM DETERMINADO PRODUTO
-		Map<Consumidor, Produto> map = new HashMap<>();
-		map.put(consumidor1, produto2);
-		map.put(consumidor2, produto4);
+		Map<Consumidor, List<Produto>> map = new HashMap<>();
 		
-		for (Map.Entry<Consumidor, Produto> entry : map.entrySet()) {
-			System.out.println(entry.getKey().getNome() + " " + entry.getValue().getNome());
-		}
+		List<Produto> produtoConsumidor1 = new ArrayList<>();
+		produtoConsumidor1.add(produto1);
+		produtoConsumidor1.add(produto2);
 		
+		
+		List<Produto> produtoConsumidor2 = new ArrayList<>();
+		produtoConsumidor2.add(produto3);
+		produtoConsumidor2.add(produto4);
+		
+		map.put(consumidor1, produtoConsumidor1);
+		map.put(consumidor2, produtoConsumidor2);
+		for (Map.Entry<Consumidor, List<Produto>> entry : map.entrySet()) {
+			System.out.println("Produtos de " + entry.getKey().getNome());
+			for (Produto produto : entry.getValue()) {
+				System.out.println(produto.getNome());
+			}
+			System.out.println();
+		}	
 		
 	}
 

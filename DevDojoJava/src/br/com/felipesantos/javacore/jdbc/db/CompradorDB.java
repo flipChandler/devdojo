f9d513c78af581656789b6b7b4e6705a5f85ba87
Felipe Santos <felipe.santos11@hotmail.com>
2020-07-15 15:33:40 -0300

    jdbc -> delete e update

--- DevDojoJava/src/br/com/felipesantos/javacore/jdbc/db/CompradorDB.java
+++ DevDojoJava/src/br/com/felipesantos/javacore/jdbc/db/CompradorDB.java
@@ -9,14 +9,54 @@ import br.com.felipesantos.javacore.jdbc.conn.ConnectionFactory;
 
 public class CompradorDB {
 	
-	public void save(Comprador comprador) {
+	public static void save(Comprador comprador) {
 		String sql = "INSERT INTO comprador (`cpf`, `nome`) VALUES ('" + comprador.getCpf() + "', '" + comprador.getNome() +"')";
 		Connection connection = ConnectionFactory.getConnection();
 		
 		try {
 			Statement stmt = connection.createStatement();
-			System.out.println(stmt.executeUpdate(sql)); 
+			stmt.executeUpdate(sql); 
 			ConnectionFactory.closeConnection(connection, stmt);
+			System.out.println("Registro inserido com sucesso");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void delete(Comprador comprador) {
+		if (comprador == null || comprador.getId() == null) {
+			System.out.println("Não foi possível excluir o registro!");
+			return;
+		}		
+		
+		String sql = "DELETE FROM comprador WHERE `id`= '" + comprador.getId() +"'";
+		Connection connection = ConnectionFactory.getConnection();
+		
+		try {
+			Statement stmt = connection.createStatement();
+			stmt.executeUpdate(sql); 
+			ConnectionFactory.closeConnection(connection, stmt);
+			System.out.println("Registro excluído com sucesso");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// atualiza, porém, se o id não existir, o método não reporta o erro
+	public static void update(Comprador comprador) {
+		if (comprador == null || comprador.getId() == null) {
+			System.out.println("Não foi possível atualizar o registro!");
+			return;
+		}		
+		
+		String sql = "UPDATE comprador SET  `cpf`= '" + comprador.getCpf() +"', `nome`='" +comprador.getNome()+"' WHERE `id`='" +comprador.getId() +"'" ;
+		Connection connection = ConnectionFactory.getConnection();
+		
+		try {
+			Statement stmt = connection.createStatement();
+			stmt.executeUpdate(sql); 
+			ConnectionFactory.closeConnection(connection, stmt);
+			System.out.println("Registro atualizado com sucesso");
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
--- DevDojoJava/src/br/com/felipesantos/javacore/jdbc/test/TesteConexao.java
+++ DevDojoJava/src/br/com/felipesantos/javacore/jdbc/test/TesteConexao.java
@@ -6,10 +6,23 @@ import br.com.felipesantos.javacore.jdbc.db.CompradorDB;
 public class TesteConexao {
 
 	public static void main(String[] args) {
-		Comprador comprador = new Comprador("159.357.951-41", "Priscila Jordan");
-		CompradorDB compradorDB = new CompradorDB();
-		compradorDB.save(comprador);
-
+		atualizar();
+	}
+	
+	public static void inserir() {
+		Comprador comprador = new Comprador("159.357.951-41", "Charlie Brown");
+		CompradorDB.save(comprador);		
+	}
+	
+	public static void deletar() {
+		Comprador comprador = new Comprador();
+		comprador.setId(4);
+		CompradorDB.delete(comprador);
+	}
+	
+	public static void atualizar() {
+		Comprador comprador = new Comprador(6,"000.000.000-00", "Mariana Baldini" );
+		CompradorDB.update(comprador);
 	}
 
 }

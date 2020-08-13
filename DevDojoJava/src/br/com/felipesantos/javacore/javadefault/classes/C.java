22fc4f4544263f174c820c407f194b5e4e6c2858
Felipe Santos <felipe.santos11@hotmail.com>
2020-08-13 10:59:27 -0300

    java 8 -> interface, métodos default e ambiguidade

--- DevDojoJava/src/br/com/felipesantos/javacore/javadefault/classes/C.java
+++ DevDojoJava/src/br/com/felipesantos/javacore/javadefault/classes/C.java
@@ -2,14 +2,19 @@ package br.com.felipesantos.javacore.javadefault.classes;
 
 import br.com.felipesantos.javacore.javadefault.interfaces.A;
 import br.com.felipesantos.javacore.javadefault.interfaces.B;
+import br.com.felipesantos.javacore.javadefault.interfaces.D;
 
-public class C implements A, B {
+public class C extends D implements A, B {
 	// uma classe sempre vai ter prioridade sobre uma interface
 	// quando tiver subinterfaces, o método mais específico será utilizado | o default de B foi utilizado
-	//
+	// quando houver ambiguidade, o método tem que ser chamado direto pelo super da interface
 	public static void main(String[] args) {
-		new C().oi(); // dentro de oi de B
+		new C().oi(); // dentro de oi de B | quando C extends D é dentro de oi de D extends A
 
+	}
+	
+	public void oi() {
+		B.super.oi();// no exemplo em que B não implements A && C implements A, B | o metodo que tem que ser chamado direto pelo super
 	}
 
 }
--- DevDojoJava/src/br/com/felipesantos/javacore/javadefault/interfaces/D.java
+++ DevDojoJava/src/br/com/felipesantos/javacore/javadefault/interfaces/D.java
@@ -1,5 +1,7 @@
 package br.com.felipesantos.javacore.javadefault.interfaces;
 
 public class D implements  A   {
-
+	public void oi () {
+		System.out.println("Dentro de oi de D extends A");
+	}
 }

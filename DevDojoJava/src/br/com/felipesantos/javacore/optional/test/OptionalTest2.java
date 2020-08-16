031d2290e92ce83c73971b017668f897f44f351f
Felipe Santos <felipe.santos11@hotmail.com>
2020-08-16 19:37:54 -0300

    java 8 -> optional, filter, flatMap, map

--- DevDojoJava/src/br/com/felipesantos/javacore/optional/test/OptionalTest2.java
+++ DevDojoJava/src/br/com/felipesantos/javacore/optional/test/OptionalTest2.java
@@ -1,5 +1,7 @@
 package br.com.felipesantos.javacore.optional.test;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 import br.com.felipesantos.javacore.optional.classes.Carro;
@@ -16,16 +18,52 @@ public class OptionalTest2 {
 		Pessoa pessoa = new Pessoa(carro, "Carlos");
 		
 		System.out.println(obterNomeSeguradora(Optional.ofNullable(pessoa)));
+		
+		checarNomeSeguradora(seguradora);
+		checarSeguradoraOptional(seguradora);
+		pessoa.setIdade(22);
+		System.out.println(obterNomeSeguradoraComIdade(pessoa, 18));
+		
+		Map<String, String> map = new HashMap<>();
+		System.out.println(Optional.ofNullable(map.get("kaka"))); // Optional.empty
+		
+		System.out.println(stringToInt("A"));// Optional.empty
 	}
 	
+	private static Optional<Integer> stringToInt(String numero){
+		try {
+			return Optional.of(Integer.parseInt(numero));
+		} catch (NumberFormatException e) {
+			return Optional.empty();
+		}
+	}
+	
+	private static void checarNomeSeguradora(Seguradora seguradora) {
+		if (seguradora != null && seguradora.getNome().equals("DevDojo Seguros")) {
+			System.out.println("é o DevDojo");
+		}
+	}
+	 	
+	private static void checarSeguradoraOptional(Seguradora seguradora) {
+		Optional.ofNullable(seguradora).filter(seg -> seg.getNome().equals("DevDojo Seguros"))
+		.ifPresent(x -> System.out.println("é o Devdojo"));
+	}
+	
+	private static String obterNomeSeguradoraComIdade(Pessoa p, int idadeMinima) {
+		return Optional.ofNullable(p)
+				.filter(pessoa -> pessoa.getIdade() >= idadeMinima)
+				.flatMap(Pessoa::getCarro)
+				.flatMap(Carro::getSeguradora)
+				.map(Seguradora::getNome)
+				.orElse("Não existe seguradora ou a idade minima não foi atingida");
+	}
 	
 	
 	public static String obterNomeSeguradora(Optional<Pessoa> pessoa) {
 		return 	pessoa.flatMap(Pessoa::getCarro)
 				.flatMap(Carro::getSeguradora)
 				.map(Seguradora::getNome)
-				.orElse("Não existe seguradora");
-				
+				.orElse("Não existe seguradora");				
 	}
 	
 	
